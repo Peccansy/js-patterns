ac3b28036ebd6b437ac60d17fe04a05d53d803cd
Albert Nikulshin <anikulshin@n-core.ru>
2017-10-18 15:02:56 +0300

    singletone pattern es6

---
+++ src/singletone.js
@@ -0,0 +1,34 @@
+// http://amanvirk.me/singleton-classes-in-es6/
+
+let instance = null;
+
+export class SingletonModuleScopedInstance {
+  constructor() {
+    if (!instance) {
+      instance = this;
+    }
+
+    return instance;
+  }
+}
+
+// http://stackoverflow.com/a/26227662/1527470
+
+const singleton = Symbol('current singleton instance');
+const singletonEnforcer = Symbol('enforcer');
+
+export class SingletonEnforcer {
+  constructor(enforcer) {
+    if (enforcer !== singletonEnforcer) {
+      throw new Error('Connot construct singleton');
+    }
+    this.implimentation = 'SingletonEnforcer';
+  }
+
+  static get instance() {
+    if (!this[singleton]) {
+      this[singleton] = new SingletonEnforcer(singletonEnforcer);
+    }
+    return this[singleton];
+  }
+}
